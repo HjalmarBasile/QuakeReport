1ec33ee48f029a5ab1b7b204dcc28d33009e6570
hjalmar <hjalmar.basile@icloud.com>
2018-06-29 19:09:27 +0200

    Create PreferenceScreen

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,8 +1,9 @@
 package com.example.android.quakereport;
 
-import android.preference.PreferenceFragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,6 +15,11 @@ public class SettingsActivity extends AppCompatActivity {
 
     public static class EarthquakePreferenceFragment extends PreferenceFragment {
 
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 
 }
