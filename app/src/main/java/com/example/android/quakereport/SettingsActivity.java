a9329996679faff76206da578b816cb8fe7e85e1
hjalmar <hjalmar.basile@icloud.com>
2018-07-01 23:19:21 +0200

    Set preference summary on every change

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -19,7 +22,28 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitudePref = findPreference(getString(R.string.settings_min_magnitude_key));
+            setPreferenceSummary(minMagnitudePref);
+
+            minMagnitudePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    String stringValue = newValue.toString();
+                    preference.setSummary(stringValue);
+                    return true;
+                }
+
+            });
         }
+
+        private void setPreferenceSummary(Preference preference) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            preference.setSummary(preferenceString);
+        }
+
     }
 
 }
